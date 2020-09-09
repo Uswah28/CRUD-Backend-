f310dd6d8b7f403d272ad6b2f382912671700d36
Uswah28 <uswatunhasanah2899@gmail.com>
2020-09-09 22:54:28 +0800

    fix commit

--- backend/models/exercise.model.js
+++ backend/models/exercise.model.js
@@ -5,7 +5,7 @@ const Schema = mongoose.Schema;
 const exerciseSchema = new Schema({
   name: { type: String, required: true },
   address: { type: String, required: true },
-  date: { type: Date, required: true },
+  date: { type: String, required: true },
   email: { type: String, required: true },
   number: { type: String, required: true },
   
